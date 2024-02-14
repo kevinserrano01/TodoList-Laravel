919d3a27bfd84282fd70c76a2660fbd4ac129dcc
Kev <kevinserrano010@gmail.com>
2024-02-14 14:37:10 -0300

    Estilizando App con bootstrap 5

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,42 +1,85 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Todo List - Laravel</title>
-</head>
-<body>
-    
-    <form action="{{ url('/') }}" method="post">
-        @csrf
-        <input type="text" name="task" id="task">
-        <input type="submit" value="Add task">
-    </form>
+    <head>
+        <title>Todo List with Laravel</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
 
-    <br>
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
 
-    <table border="1">
-        <tr>
-            <td> Nombre de la tarea </td>
-            <td> Accion </td>
-        </tr>
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main class="container">
+            <br>
+            <div class="card">
+                <div class="card-header">Tareas</div>
+                    <div class="card-body">
+                        <form action="{{ url('/') }}" method="post">
+                            @csrf
+                            Tarea:
+                            <input type="text" class="form-control" name="task" id="task">
+                            <br>
+                            <input type="submit" class="btn btn-primary" value="Add task">
+                        </form>
 
-        @foreach ($tasks as $task)
-            <tr>
-            <td> {{ $task->task }} </td>
-            <td>
-                <form action="{{ route('task.destroy', $task->id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" value="X">
-                </form>
-            </td>
-        </tr>
-        @endforeach
-        
+                        <br>
 
-    </table>
+                        <div class="table-responsive-sm table-bordered table-striped">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Accion</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($tasks as $task)
+                                        <tr class="">
+                                            <td>{{ $task->task }}</td>
+                                            <td scope="row">
+                                                <form action="{{ route('task.destroy', $task->id) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <input type="submit" class="btn btn-success" value="Realizada">
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                <div class="card-footer text-muted"></div>
+            </div>
 
-</body>
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
